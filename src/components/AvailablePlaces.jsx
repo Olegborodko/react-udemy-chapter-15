e978619a85f257c160205a5d852f3c1982d79d31
borodkoleg <borodkoleg@gmail.com>
2024-09-03 13:01:17 +0300

    Network select 3G, add text - Fetching place data...

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,15 +3,19 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsFetching(true);
+
         const response = await fetch('http://localhost:3000/places');
         const data = await response.json();
         console.log('data' , data);
-        setAvailablePlaces(data.places)
+        setAvailablePlaces(data.places);
+        setIsFetching(false);
       } catch (error) {
         console.error('Ошибка при загрузке данных:', error);
       }
@@ -25,6 +29,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
       title="Available Places"
       places={availablePlaces}
       fallbackText="No places available."
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       onSelectPlace={onSelectPlace}
     />
   );
