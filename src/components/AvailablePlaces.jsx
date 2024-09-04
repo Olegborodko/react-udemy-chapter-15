73128e5171153f690c2f30d271f102173d513145
borodkoleg <borodkoleg@gmail.com>
2024-09-04 17:48:42 +0300

    fetch response, try catch, status code

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
+import ErrorCom from './ErrorCom.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,16 +16,28 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
         const response = await fetch('http://localhost:3000/places');
         const data = await response.json();
-        console.log('data' , data);
-        setAvailablePlaces(data.places);
-        setIsFetching(false);
+
+        if (!response.ok) {
+          throw new Error('Failded to fetch places');
+        }
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(data.places, position.coords.latitude, position.coords.longitude)
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
-        console.error('Ошибка при загрузке данных:', error);
+        setError(error);
+        setIsFetching(false);
       }
     }
 
     fetchData();
   }, [])
+
+  if (error) {
+    return <ErrorCom title="Error" message={error.message} />
+  }
 
   return (
     <Places
